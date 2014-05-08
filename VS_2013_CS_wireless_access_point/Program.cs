51aa7e6260762c1541f959fba5e39ce4ce1e2ba7
amdreallyfast <amdreallyfast@hotmail.com>
2014-05-08 12:49:22 -0700

    moved socket experiments to another project
    I moved the socket experiments out of the "C# wireless AP" project and
    into it's own.  Those sockets didn't have anything to do with the
    wireless AP problem (not yet, at least).
    
    Also modified gitignore to expand its rules to the root directory of
    each project folder.  Previously, each project folder was specified by
    name.  Now the project folders and generically accessed.

--- VS_2013_CS_wireless_access_point/Program.cs
+++ VS_2013_CS_wireless_access_point/Program.cs
@@ -4,61 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using System.Net;
-using System.Net.Sockets;
-using System.IO;
-
 namespace VS_2013_CS_wireless_access_point
 {
-   class ShowIP
-   {
-      //public static void Main(string[] args)
-      public static void show_IP_main()
-      {
-         //string name = (args.Length < 1) ? Dns.GetHostName() : args[0];
-         string name = Dns.GetHostName();
-         try
-         {
-            //IPAddress[] addrs = Dns.Resolve(name).AddressList;
-            IPAddress[] addrs = Dns.GetHostEntry(name).AddressList;
-            foreach (IPAddress addr in addrs)
-               Console.WriteLine("{0}/{1}", name, addr);
-         }
-         catch (Exception e)
-         {
-            Console.WriteLine(e.Message);
-         }
-      }
-   }
-
    class Program
    {
       static void Main(string[] args)
       {
-         //ShowIP.show_IP_main();
-
-         TcpClient client = new TcpClient("10.10.10.126", 5);
-         Stream s = client.GetStream();
-         StreamReader sr = new StreamReader(s);
-         StreamWriter sw = new StreamWriter(s);
-         sw.AutoFlush = true;
-
-         string my_string = Console.ReadLine();
-         //string new_string = String.Format("--You wrote: '{0}'", my_string);
-         //Console.WriteLine(new_string);
-
-         sw.WriteLine(my_string);
-         my_string = sr.ReadLine();
-         while("" == my_string)
-         {
-            my_string = sr.ReadLine();
-         }
-         string new_string = String.Format("--You wrote: '{0}'", my_string);
-
-         Console.WriteLine(new_string);
-
-         s.Close();
-         client.Close();
+         Console.WriteLine("hello world!");
       }
    }
 }
